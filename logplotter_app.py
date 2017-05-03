e6f7b190f425a6a4cdae16d9c3e461bcec976137
kylemos <moslk@msn.com>
2017-05-03 22:30:19 +0100

    update

--- logplotter_app.py
+++ logplotter_app.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 
 import Tkinter as tk
-import ttk as ttk  # basically the CSS for tkinter!
-
+import ttk as ttk
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.figure import Figure
+from matplotlib import style
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
                                                NavigationToolbar2TkAgg)
+import json # temporary, in place of db
+from functools import partial  # <-- Genius tool!!!
 
+style.use('bmh')
+
+"""
+TODO:
+ * Use matplotlib's 'set_data' method to update graph
+ * 
+"""
 
 class LogPlotterApp(tk.Tk):
 
@@ -39,37 +48,63 @@ class LogPlotterApp(tk.Tk):
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
+    
+        # setup frame
         tk.Frame.__init__(self, parent)
         self.master = controller
-        self.init_window()
-        
-    def init_window(self):
-        # add title
-        self.master.title("Logplotter application")
+        self.master.title("Logplotter")
         self.pack(fill="both", expand=1)
+        
+        # load list of holes
+        with open('holes.json','r') as fp:
+            holes = json.load(fp)
         
         # add a menu bar
         menu = tk.Menu(self.master)
         self.master.config(menu=menu)
-        file, edit = tk.Menu(menu, tearoff=False), tk.Menu(menu, tearoff=False)
-        file.add_command(label="Exit", command=self.client_exit)
-        edit.add_command(label="Display log", command=self.display_log)
-        menu.add_cascade(label="File", menu=file)
-        menu.add_cascade(label="Edit", menu=edit)
+        # file menu
+        fileMenu = tk.Menu(menu, tearoff=False)
+        fileMenu.add_command(label="Save as image")
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command=self.client_exit)
+        # view menu
+        viewMenu = tk.Menu(menu, tearoff=False)
+        viewMenu.add_command(label="Set background colour")
+        # view submenu: display log
+        logMenu = tk.Menu(viewMenu, tearoff=False)
+        for hole in holes:
+            logMenu.add_command(label=hole, command=partial(self.display_log,hole))
+        viewMenu.add_cascade(label="Display Log", menu=logMenu)
+        # add submenus to menu bar
+        menu.add_cascade(label="File", menu=fileMenu)
+        menu.add_cascade(label="View", menu=viewMenu)
         
-    # menu functions
-    def display_log(self):
+        
+        
+    # menu methods
+    def display_log(self, hole):
+    
+        print hole
+    
+        # step1: get data from db
+        
+        # step2: update graph
         f = Figure(figsize=(5,5), dpi=100)
         f.patch.set_facecolor('w')
         ax = f.add_subplot(111)
-        ax.plot([1,2,5,3,7,8],[5,4,9,3,2,7])
+        if hole=='OL-KR1':
+            ax.plot([1,2,3,4,5,6],[5,4,9,3,2,7])
+        elif hole=='OL-KR2':
+            ax.plot([1,2,3,4,5,6],[2,6,9,9,1,4])
+        else:
+            ax.plot([1,2,3,4,5,6],[7,6,2,5,1,8])
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         
     def client_exit(self):
         exit()
-
+        
         
 # main loop
 if __name__ == '__main__':
