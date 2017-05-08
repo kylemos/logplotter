43d43146ca9756b6b81d0c990c84da3f71f68e0b
kylemos <moslk@msn.com>
2017-05-08 22:32:19 +0100

    further updates

--- logplotter_app.py
+++ logplotter_app.py
@@ -2,21 +2,28 @@
 
 import Tkinter as tk
 import ttk as ttk
+from tkColorChooser import askcolor
+from tkFileDialog import asksaveasfilename
+
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.figure import Figure
 from matplotlib import style
+from matplotlib.figure import Figure
+#from matplotlib.lines import Line2D
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
                                                NavigationToolbar2TkAgg)
+from functools import partial
+import time
 import json # temporary, in place of db
-from functools import partial  # <-- Genius tool!!!
 
 style.use('bmh')
 
 """
 TODO:
- * Use matplotlib's 'set_data' method to update graph
- * 
+ * MVC design pattern!
+ * Use matplotlib's 'set_data' method to update graph?
+ * Improve save_as_image method (filename list, etc...) - see https://tkinter.unpythonic.net/wiki/tkFileDialog
+ * Add live-updating statusbar
 """
 
 f = Figure(figsize=(5,5), dpi=100)
@@ -25,33 +32,51 @@ ax = f.add_subplot(111)
 
 class LogPlotterApp(tk.Tk):
 
+    '''Application-level controller class'''
+
     def __init__(self):
     
-        tk.Tk.__init__(self)
-        container = tk.Frame(self)
+        '''initialiser'''
         
+        tk.Tk.__init__(self)
+        
+        # set up container frame
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
-        frame = StartPage(container, self)
-        frame.grid(row=0, column=0, sticky="nsew")
-        
-        # dict to hold frames
+        # store page classes in dict
         self.frames = {}
-        self.frames[StartPage] = frame
+        for F in [ViewPage]:
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
         
-        self.show_frame(StartPage)
+        # (default) show ViewPage
+        self.show_frame(ViewPage)
         
     def show_frame(self, cont):
     
+        '''Raise page'''
+        
         frame = self.frames[cont]
         frame.tkraise()
         
-                                               
-class StartPage(tk.Frame):
+    def exit(self):
+    
+        '''Quit application'''
+        
+        root.destroy()
+        
+
+class ViewPage(tk.Frame):
+
+    '''Viewer class'''
 
     def __init__(self, parent, controller):
+    
+        '''initialiser'''
     
         # setup frame
         tk.Frame.__init__(self, parent)
@@ -68,9 +93,9 @@ class StartPage(tk.Frame):
         self.master.config(menu=menu)
         # file menu
         fileMenu = tk.Menu(menu, tearoff=False)
-        fileMenu.add_command(label="Save as image")
+        fileMenu.add_command(label="Save as image", command=self.save_as_image)
         fileMenu.add_separator()
-        fileMenu.add_command(label="Exit", command=self.client_exit)
+        fileMenu.add_command(label="Exit", command=controller.exit)
         # view menu
         viewMenu = tk.Menu(menu, tearoff=False)
         viewMenu.add_command(label="Set background colour", command=self.change_background)
@@ -88,29 +113,49 @@ class StartPage(tk.Frame):
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         
+    def save_as_image(self):
+    
+        '''Save figure as image'''
         
-    # menu methods
+        savename = asksaveasfilename(defaultextension='.png')
+        if savename:
+            f.savefig(savename, dpi=150)
+        
     def display_log(self, hole):
+    
+        '''Select log to display'''
+        
+        # display load screen
+        # w = self.canvas.create_text(text="Loading...", font=("Helvetica", 16))
+        # time.sleep(2)
     
         # step1: get data from db
         with open('data.json','r') as fp:
             data = json.load(fp)
         xs,ys = data.get(hole)
+        # w.delete()
         
-        # step2: update graph
-        # f = Figure(figsize=(5,5), dpi=100)
-        # f.patch.set_facecolor('w')
-        # ax = f.add_subplot(111)
-        
-        ax.cla()
+        #line1.set_data(xs,ys)
+        ax.clear()
         ax.plot(xs,ys,'r')
         self.canvas.draw()
         
     def change_background(self):
+    
+        '''Change figure background'''
         
+        color = askcolor(initialcolor="#ffffff", title="Choose background colour")
+        f.patch.set_facecolor(color[1])
+        self.canvas.draw()
         
-    def client_exit(self):
-        exit()
+
+class Model(object):
+    
+    '''Model class'''
+    
+    def __init__(self):
+        pass
+    
         
         
 # main loop
